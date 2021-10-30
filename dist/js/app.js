12ab8399f4021fa673fcbfecd660f595d857c658
mitrovicbojan <79321143+mitrovicbojan@users.noreply.github.com>
2021-10-30 21:25:32 +0300

    nav

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,27 +1,4 @@
-const menuBtn = document.querySelector('.menu-btn');
-const hamburger = document.querySelector('.menu-btn__burger');
-const nav = document.querySelector('.nav');
-const menuNav = document.querySelector('.menu-nav');
-const navItems = document.querySelectorAll('.menu-nav__item');
+var tglbtn = document.getElementById("nav-toggle");
+var navlst = document.getElementById("nav-list")
 
-let showMenu = false;
-
-menuBtn.addEventListener('click', toggleMenu);
-
-function toggleMenu() {
-    if(!showMenu) {
-        hamburger.classList.add('open');
-        nav.classList.add('open');
-        menuNav.classList.add('open');
-        navItems.forEach(item => item.classList.add('open'));
-
-        showMenu = true;
-    } else {
-        hamburger.classList.remove('open');
-        nav.classList.remove('open');
-        menuNav.classList.remove('open');
-        navItems.forEach(item => item.classList.remove('open'));
-
-        showMenu = false;
-    }
-}
+tglbtn.addEventListener('click', () => {navlst.classList.toggle("active");});
